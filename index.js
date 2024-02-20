3e60d7dc665c4a4389b3b70eb984291cb8f16d6d
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-02-20 23:10:31 +0900

    Properly add/remove JSON RPC Content-Length header

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const http = require('http');
 
-process.stdin.on('data', (data) => {
+let collectedData = '';
+
+process.stdin.on('data', async (data) => {
+  collectedData += data;
+  const message = findNextJSONRPCMessage(collectedData);
+  if (message) {
+    sendMessageToServer(message.message, (response) => {
+      process.stdout.write(
+        `Content-Length: ${response.length}\r\n` +
+        '\r\n' +
+        response
+      );
+    });
+    collectedData = message.remaining;
+  }
+});
+
+const findNextJSONRPCMessage = (string) => {
+  const contentLengthMatch = string.match(/Content-Length: (\d+)/);
+  if (!contentLengthMatch) {
+    return null;
+  }
+
+  const contentLength = parseInt(contentLengthMatch[1], 10);
+  const contentStart = string.indexOf('\r\n\r\n') + 4;
+  const contentEnd = contentStart + contentLength;
+
+  if (string.length < contentEnd) {
+    return null;
+  }
+
+  return {
+    message: string.slice(contentStart, contentEnd),
+    remaining: string.slice(contentEnd),
+  };
+};
+
+const sendMessageToServer = (message, processResponseCallback) => {
   const request = http.request(
     'http://localhost:9001',
     { method: 'POST' },
@@ -12,11 +49,12 @@ process.stdin.on('data', (data) => {
       });
 
       response.on('end', () => {
-        process.stdout.write(Buffer.concat(bodyChunks));
+        const body = Buffer.concat(bodyChunks).toString();
+        processResponseCallback(body);
       });
     }
   );
   request.setHeader('Content-Type', 'application/json');
-  request.write(data);
+  request.write(message);
   request.end();
-});
+}
--- test.js
+++ test.js
@@ -5,17 +5,22 @@ const { startStubServer, startBridgeProcess, waitForNextRequest, readStringSync 
 (async () => {
   const server = startStubServer(9001, ['{"response": "ok"}']);
   const bridgeProcess = startBridgeProcess();
-  bridgeProcess.stdin.write('{"content": "hello"}');
+  bridgeProcess.stdin.write('Content-Length: 19\r\n' +
+                            '\r\n' +
+                            '{"content":"hello"}');
 
   await waitForNextRequest(server);
 
   assert.deepStrictEqual(server.receivedRequests, [
-    { method: 'POST', body: '{"content": "hello"}' },
+    { method: 'POST', body: '{"content":"hello"}' },
   ]);
 
   const response = await readStringSync(bridgeProcess.stdout);
 
-  assert.strictEqual(response, '{"response": "ok"}');
+  assert.strictEqual(response,
+                     'Content-Length: 18\r\n' +
+                     '\r\n' +
+                     '{"response": "ok"}');
 
   bridgeProcess.kill();
 })();
