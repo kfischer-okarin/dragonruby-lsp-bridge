2284e001d27bd99698a0e3e08aae102060670bab
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-03-15 21:46:10 +0900

    Extract actual forwarding logic into different object

---
+++ jsonRpcForwarder.js
@@ -0,0 +1,160 @@
+const http = require('http');
+
+exports.buildJsonRpcForwarder = () => new JsonRpcForwarder();
+
+class JsonRpcForwarder {
+  constructor() {
+    this.storedInitializeMessage = null;
+    this.returnedInitializeResponse = false;
+    this.collectedData = '';
+    this.tryInitializeMessageInterval = null;
+  }
+
+  processIncomingData(data) {
+    this.collectedData += data;
+    const message = extractNextJSONRPCMessage(this.collectedData);
+    if (message) {
+      if (isInitializeMessage(message.message)) {
+        this.startNewEditorSession(message.message);
+      }
+
+      this.postJSONRPCMessageToServer(
+        message.message,
+        {
+          onConnectionRefused: () => {
+            if (this.storedInitializeMessage && !this.tryingToConnectToServer) {
+              this.tryToConnectToServer();
+            }
+          },
+        },
+      );
+      this.collectedData = message.remaining;
+    }
+  }
+
+  startNewEditorSession(initializeMessage) {
+    this.storedInitializeMessage = initializeMessage;
+    this.returnedInitializeResponse = false;
+  }
+
+  postJSONRPCMessageToServer(message, { onConnectionRefused }) {
+    this.postToServer(
+      message,
+      {
+        onResponse: (response) => {
+          if (response.status === 204) {
+            return;
+          }
+
+          const wasInitializeMessage = isInitializeMessage(message);
+          const shouldReturnResponse = !wasInitializeMessage || !this.returnedInitializeResponse;
+          if (shouldReturnResponse) {
+            process.stdout.write(
+              `Content-Length: ${response.body.length}\r\n` +
+                '\r\n' +
+                response.body
+            );
+
+            if (wasInitializeMessage) {
+              this.returnedInitializeResponse = true;
+            }
+          }
+        },
+        onError: (error) => {
+          if (error.code === 'ECONNREFUSED' && onConnectionRefused) {
+            onConnectionRefused();
+            return;
+          }
+          throw error;
+        },
+      },
+    );
+  }
+
+  postToServer(requestBody, { onResponse, onError }) {
+    const request = http.request(
+      'http://localhost:9001/dragon/lsp',
+      { method: 'POST' },
+      (response) => {
+        const bodyChunks = [];
+
+        response.on('data', (chunk) => {
+          bodyChunks.push(chunk);
+        });
+
+        response.on('end', () => {
+          const body = Buffer.concat(bodyChunks).toString();
+          onResponse({
+            status: response.statusCode,
+            body,
+          });
+        });
+      }
+    );
+
+    request.on('error', onError);
+    request.setHeader('Content-Type', 'application/json');
+    request.write(requestBody);
+    request.end();
+  }
+
+  get tryingToConnectToServer() {
+    return this.tryInitializeMessageInterval !== null;
+  }
+
+  tryToConnectToServer() {
+    this.tryInitializeMessageInterval = setInterval(
+      tryToSendInitializeMessage,
+      500,
+      this
+    );
+  };
+
+  stopConnectingToServer() {
+    clearInterval(this.tryInitializeMessageInterval);
+    this.tryInitializeMessageInterval = null;
+  }
+}
+
+// --- private functions ---
+
+const extractNextJSONRPCMessage = (string) => {
+  const headerMatch = string.match(/Content-Length: (\d+)\r\n\r\n/);
+  if (!headerMatch) {
+    return null;
+  }
+
+  const contentLength = parseInt(headerMatch[1], 10);
+  const contentStart = headerMatch.index + headerMatch[0].length;
+  const contentEnd = contentStart + contentLength;
+
+  if (string.length < contentEnd) {
+    return null;
+  }
+
+  return {
+    message: string.slice(contentStart, contentEnd),
+    remaining: string.slice(contentEnd),
+  };
+};
+
+const isInitializeMessage = (message) => {
+  const parsedMessage = JSON.parse(message);
+  return parsedMessage.method === 'initialize';
+};
+
+const tryToSendInitializeMessage = (forwarder) => {
+  let success = true;
+  forwarder.postJSONRPCMessageToServer(
+    forwarder.storedInitializeMessage,
+    {
+      onConnectionRefused: () => {
+        success = false;
+      },
+    },
+  );
+
+  if (success) {
+    forwarder.stopConnectingToServer();
+  }
+};
