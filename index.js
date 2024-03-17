219323c84be7cd3b6c2df58ec0cef09774e6f14f
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-03-17 16:31:57 +0900

    Wrap the whole script main into async method

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const { parseOptions } = require('./lib/cli');
 const { buildLspMessageRelay } = require('./lib/lspMessageRelay');
 
-const options = parseOptions(process.argv);
-const relay = buildLspMessageRelay(options);
+const main = async () => {
+  const options = parseOptions(process.argv);
+  const relay = buildLspMessageRelay(options);
 
-process.stdin.on('data', async (data) => {
-  relay.processIncomingData(data.toString());
-});
+  process.stdin.on('data', async (data) => {
+    relay.processIncomingData(data.toString());
+  });
 
-process.on('SIGTERM', () => {
-  relay.shutdown();
-  process.exit(0);
-});
+  process.on('SIGTERM', () => {
+    relay.shutdown();
+    process.exit(0);
+  });
+};
+
+main();
